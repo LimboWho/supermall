9521d2997ac1e6c38e2a798e05d009f30f948b59
limbo_gitee <1970668119@qq.com>
2020-07-30 19:46:24 +0800

    项目文件配置

---
+++ src/vue.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    configureWebpack:{
+        resole:{
+            alias:{
+                'assets':'@/assets',
+                'common':'@/common',
+                'components':'@/components',
+                'network':'@/network',
+            }
+        }
+    }
+}
